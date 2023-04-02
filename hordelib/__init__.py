0ab81000330273fe41f3acdca10ddbdbc4ef1c48
Jug <jugdev@proton.me>
2023-04-02 18:07:03 +0100

    fix: include dependencies in pypi package.

--- hordelib/__init__.py
+++ hordelib/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 from . import install
 
-VERSION = "0.0.6"
+VERSION = "0.0.7"
 COMFYUI_VERSION = "27fc64ad469c07d8f84b2c2791a593f1cf2c7b59"
 
 current_file_path = os.path.abspath(__file__)
