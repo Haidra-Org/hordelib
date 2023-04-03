21a824924e7fa3b6a9015a26aeddf99d3d7c38b3
Divided by Zer0 <mail@dbzer0.com>
2023-04-03 17:44:14 +0200

    feat: Adds support for using a Model Manager  (#8)

--- hordelib/__init__.py
+++ hordelib/__init__.py
@@ -1,6 +1,7 @@
 import os
 import sys
-from . import install
+from hordelib import install
+from hordelib.utils.switch import Switch
 
 VERSION = "0.0.9"
 COMFYUI_VERSION = "27fc64ad469c07d8f84b2c2791a593f1cf2c7b59"
@@ -14,5 +15,18 @@ installer = install.Installer()
 installer.install(COMFYUI_VERSION)
 
 
+disable_xformers = Switch()
+disable_voodoo = Switch()
+enable_local_ray_temp = Switch()
+disable_progress = Switch()
+disable_download_progress = Switch()
+enable_ray_alternative = Switch()
+
+horde_model_manager = None # This needs
+
 class HordelibException(Exception):
     pass
+
+def set_horde_model_manager(mm):
+    global horde_model_manager
+    horde_model_manager = mm
---
+++ hordelib/model_manager/compvis.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import time
+import comfy
+from pathlib import Path
+
+from hordelib.cache import get_cache_directory
+from hordelib.model_manager.base import BaseModelManager
+
+from loguru import logger
+
+
+class CompVisModelManager(BaseModelManager):
+    def __init__(self, download_reference=True, custom_path="models/custom"):
+        super().__init__()
+        self.download_reference = download_reference
+        self.path = f"{get_cache_directory()}/compvis"
+        self.custom_path = custom_path
+        self.models_db_name = "stable_diffusion"
+        self.models_path = self.pkg / f"{self.models_db_name}.json"
+        self.remote_db = (
+            f"https://raw.githubusercontent.com/db0/AI-Horde-image-model-reference/main/{self.models_db_name}.json"
+        )
+        self.init()
+
+    def load(
+        self,
+        model_name: str,        
+        output_vae=True, 
+        output_clip=True,
+    ):
+        """
+        model_name: str. Name of the model to load. See available_models for a list of available models.
+        """
+        if model_name not in self.models:
+            logger.error(f"{model_name} not found")
+            return False
+        if model_name not in self.available_models:
+            logger.error(f"{model_name} not available")
+            self.download_model(model_name)
+            logger.init_ok(f"{model_name}", status="Downloaded")
+        if model_name not in self.loaded_models:
+            tic = time.time()
+            logger.init(f"{model_name}", status="Loading")
+            embeddings_path = os.getenv("HORDE_MODEL_DIR_EMBEDDINGS", "./")
+            ckpt_path = self.get_model_files(model_name)[0]["path"]
+            ckpt_path = f"{self.path}/{ckpt_path}"
+            self.loaded_models[model_name] = comfy.sd.load_checkpoint_guess_config(
+                ckpt_path,
+                output_vae=True,
+                output_clip=True,
+                embedding_directory=embeddings_path,
+            )
+            toc = time.time()
+            logger.init_ok(f"{model_name}: {round(toc-tic,2)} seconds", status="Loaded")
+            return True
