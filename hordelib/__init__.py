fda9ee8599ae08176e098a16654530c00c1617bc
Jug <jugdev@proton.me>
2023-04-06 12:11:02 +0100

    Revert "chore: upstream comfyui update"
    This reverts commit 80157d5257d3bf71a28fe8e462de7639d0de75f6.

--- hordelib/__init__.py
+++ hordelib/__init__.py
@@ -3,7 +3,7 @@ from hordelib.config_path import set_system_path
 from hordelib.settings import WorkerSettings
 
 VERSION = "0.0.10"
-COMFYUI_VERSION = "0bb5f93b9291f088de8eee44cd98db86b44b1e6d"
+COMFYUI_VERSION = "1718730e80549c35ce3c5d3fb7926ce5654a2fdd"
 
 set_system_path()
 
