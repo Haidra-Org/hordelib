a84b7b2c65acefcad165480853d903ceb852d512
Jug <124293863+jug-dev@users.noreply.github.com>
2023-04-12 13:28:31 +0100

    fix: suppress terminal spam (#104)

---
+++ hordelib/utils/ioredirect.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+from loguru import logger
+
+
+class OutputCollector:
+    def __init__(self):
+        logger.disable("tqdm")  # just.. no
+        self.deque = deque()
+
+    def write(self, s):
+        if s != "\n":
+            self.deque.append(s.strip())
+
+    def set_size(self, size):
+        while len(self.deque) > size:
+            self.deque.popleft()
+
+    def flush(self):
+        pass
+
+    def isatty(self):
+        # No, we are not a TTY
+        return False
+
+    def close(self):
+        pass
+
+    def replay(self):
+        while len(self.deque):
+            logger.debug(self.deque.popleft())
