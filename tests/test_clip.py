7596777f39eed9827307a5706c90e0f5c1e17d5f
Divided by Zer0 <mail@dbzer0.com>
2023-04-13 10:34:10 +0200

    feat: Clip Rankings (#117)

--- tests/test_clip.py
+++ tests/test_clip.py
@@ -8,11 +8,11 @@ from hordelib.shared_model_manager import SharedModelManager
 
 
 class TestHordeClip:
-    @pytest.fixture(autouse=True)
+    @pytest.fixture(autouse=True, scope="class")
     def setup_and_teardown(self):
-        self.horde = HordeLib()
+        TestHordeClip.horde = HordeLib()
 
-        self.default_model_manager_args = {
+        TestHordeClip.default_model_manager_args = {
             # aitemplate
             # "blip": True,
             "clip": True,
@@ -24,17 +24,17 @@ class TestHordeClip:
             # "gfpgan": True,
             # "safety_checker": True,
         }
-        self.image = Image.open("images/test_db0.jpg")
+        TestHordeClip.image = Image.open("images/test_db0.jpg")
         SharedModelManager.loadModelManagers(**self.default_model_manager_args)
         assert SharedModelManager.manager is not None
         SharedModelManager.manager.load("ViT-L/14")
         yield
-        del self.horde
+        del TestHordeClip.horde
         SharedModelManager._instance = None
         SharedModelManager.manager = None
 
     def test_clip_similarities(self):
-        assert SharedModelManager.manager.clip.is_model_loaded("ViT-L/14") is True
+        assert SharedModelManager.manager.is_model_loaded("ViT-L/14") is True
         word_list = ["outlaw", "explosion", "underwater"]
         model_info = SharedModelManager.manager.loaded_models["ViT-L/14"]
         interrogator = Interrogator(model_info)
@@ -47,3 +47,15 @@ class TestHordeClip:
         assert similarity_result["default"]["outlaw"] > 0.15
         assert similarity_result["default"]["explosion"] > 0.15
         assert similarity_result["default"]["underwater"] < 0.15
+
+    def test_clip_rankings(self):
+        assert SharedModelManager.manager.is_model_loaded("ViT-L/14") is True
+        model_info = SharedModelManager.manager.loaded_models["ViT-L/14"]
+        interrogator = Interrogator(model_info)
+        ranking_result = interrogator(
+            image=self.image,
+            rank=True,
+        )
+        assert type(ranking_result) is dict
+        assert "artists" in ranking_result
+        assert "mediums" in ranking_result
