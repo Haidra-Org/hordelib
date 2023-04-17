20c4deb5fd34af937ceeb59019fdf0e32c17dd50
Jug <jugdev@proton.me>
2023-04-17 23:13:47 +0100

    fix: don't mix up controlnets and endlessly load them into VRAM

--- hordelib/nodes/node_controlnet_model_loader.py
+++ hordelib/nodes/node_controlnet_model_loader.py
@@ -2,9 +2,12 @@ import os
 
 import comfy
 from loguru import logger
+import threading
 
 
 class HordeDiffControlNetLoader:
+    _mutex = threading.Lock()
+
     @classmethod
     def INPUT_TYPES(s):
         return {
@@ -27,12 +30,13 @@ class HordeDiffControlNetLoader:
             logger.error("controlnet model_manager appears to be missing!")
             raise RuntimeError  # XXX better guarantees need to be made
 
-        merged_model = model_manager.manager.controlnet.merge_controlnet(
-            control_net_name,
-            model,
-        )
+        with HordeDiffControlNetLoader._mutex:
+            merged_model = model_manager.manager.controlnet.merge_controlnet(
+                control_net_name,
+                model,
+            )
 
-        return merged_model  # noqa: RET504
+            return merged_model  # noqa: RET504
 
 
 NODE_CLASS_MAPPINGS = {"HordeDiffControlNetLoader": HordeDiffControlNetLoader}
