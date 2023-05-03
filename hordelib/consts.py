bd7b08213d8c7dcc9bdf47a2e00d35977ffb26ed
tazlin <tazlin.on.github@gmail.com>
2023-05-03 15:26:36 -0400

    fix: remove ether real from exclude list

--- hordelib/consts.py
+++ hordelib/consts.py
@@ -20,7 +20,7 @@ class HordeSupportedBackends(Enum):
 
 # Models Excluded from hordelib (for now) # FIXME
 # this could easily be a json file on the AI-Horde-image-model-reference repo
-EXCLUDED_MODEL_NAMES = ["Ether Real Mix", "pix2pix", "anything_v4_inpainting", "dreamlike_diffusion_inpainting"]
+EXCLUDED_MODEL_NAMES = ["pix2pix", "anything_v4_inpainting", "dreamlike_diffusion_inpainting"]
 
 
 class MODEL_CATEGORY_NAMES(str, Enum):
