389c6bc3d7cf790ccbb3e6d756d5b948a0f6eb52
Jug <jugdev@proton.me>
2023-05-11 22:23:29 +0100

    test: add 10 step sampler tests

--- tests/test_horde_samplers.py
+++ tests/test_horde_samplers.py
@@ -5,6 +5,8 @@ from PIL import Image
 from hordelib.horde import HordeLib
 from hordelib.shared_model_manager import SharedModelManager
 
+SLOW_SAMPLERS = ["k_dpmpp_2s_a", "k_dpmpp_sde", "k_heun", "k_dpm_2", "k_dpm_2_a"]
+
 
 class TestHordeSamplers:
     @pytest.fixture(autouse=True, scope="class")
@@ -48,4 +50,35 @@ class TestHordeSamplers:
             data["sampler_name"] = sampler
             pil_image = self.horde.basic_inference(data)
             assert pil_image is not None
-            pil_image.save(f"images/horde_sampler_{sampler}.webp", quality=90)
+            pil_image.save(f"images/horde_sampler_30_steps_{sampler}.webp", quality=90)
+
+    def test_samplers(self):
+        data = {
+            "sampler_name": "",
+            "cfg_scale": 6.5,
+            "denoising_strength": 1.0,
+            "seed": 3688490319,
+            "height": 512,
+            "width": 512,
+            "karras": True,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": None,
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": (
+                "a woman closeup made out of metal, (cyborg:1.1), realistic skin, (detailed wire:1.3), "
+                "(intricate details), hdr, (intricate details, hyperdetailed:1.2), cinematic shot, "
+                "vignette, centered"
+            ),
+            "ddim_steps": 10,
+            "n_iter": 1,
+            "model": "Deliberate",
+        }
+        assert self.horde is not None
+        for sampler in SLOW_SAMPLERS:
+            data["sampler_name"] = sampler
+            pil_image = self.horde.basic_inference(data)
+            assert pil_image is not None
+            pil_image.save(f"images/horde_sampler_10_steps_{sampler}.webp", quality=90)
