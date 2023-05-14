b98b9921600aa77a984812e07c021dc5e6db8e28
Jug <jugdev@proton.me>
2023-05-14 12:33:27 +0100

    feat: adds code to generate all models test page

---
+++ examples/make_index_all_models.py
@@ -0,0 +1,112 @@
+# make_index_all_models.py
+# Create an index of the all model showcase (see examples.run_all_models)
+import datetime
+import glob
+import os
+
+TEMPLATE = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>hordelib</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap">
+  <style>
+    body {
+        background-color: #333;
+        color: #fff;
+    }
+    a {
+        color: #fff;
+        text-decoration: none;
+    }
+    .gallery {
+        display: flex;
+        flex-wrap: wrap;
+        /*gap: 10px;*/
+    }
+    h1 {
+        font-family: 'Roboto', sans-serif;
+        font-size: 24px;
+        text-align: left;
+        margin-bottom: 20px;
+    }
+    p {
+        font-family: 'Roboto', sans-serif;
+        font-size: 14px;
+        text-align: left;
+        margin-bottom: 20px;
+    }
+    h2 {
+        font-family: 'Roboto', sans-serif;
+        font-size: 14px;
+        text-align: left;
+        margin-bottom: 20px;
+    }
+    .thumbnail {
+        max-width: 160px;
+        cursor: pointer;
+    }
+    figure {
+        max-width: 160px;
+        text-align: center;
+        margin: 0 5px;
+    }
+    figcaption {
+        font-family: 'Roboto', sans-serif;
+        font-size: 12px;
+        color: #fff;
+        word-wrap: break-word; /* Force text to wrap onto multiple lines */
+    }
+  </style>
+</head>
+<body>
+  <h1>Test of Each Model</h1>
+  <h2>[timestamp] (UTC)</h2>
+  <p><i>Note that this image set is <b>not</b> updated automatically as part of the unit tests.
+  It has periodic manual updates due to the GPU time required.</i></p>
+  <div class="gallery">
+    [images]
+  </div>
+  <h1>Prompt and Settings</h1>
+  <p>"a woman closeup made out of metal, (cyborg:1.1), realistic skin, detailed wire:1.3), (intricate details),
+  hdr, (intricate details, hyperdetailed:1.2), cinematic shot, vignette, centered"</p>
+  <p>Seed: 3688490319, Karras, 512x512, k_euler, 30 steps, CFG 6.5</p>
+</body>
+</html>
+"""
+
+
+def href(filename):
+    return (
+        f'<a href="{filename}"><figure><img class="thumbnail" src="{filename}">'
+        f'<figcaption>{filename.replace(".webp", "")}</figcaption></figure></a>'
+    )
+
+
+def create_index():
+    # Get a list of images
+    files = glob.glob("images/all_models/*.webp")
+    refs = []
+    for imagefile in files:
+        filename = os.path.basename(imagefile)
+        refs.append(href(filename))
+    in_refs = []
+
+    # Poor man's template engine :)
+    refs.sort()
+    indexhtml = TEMPLATE.replace("[images]", "".join(refs))
+    indexhtml = indexhtml.replace("[input_images]", "".join(in_refs))
+    indexhtml = indexhtml.replace(
+        "[timestamp]",
+        datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+    )
+
+    # Output results
+    with open("images/all_models/index.html", "w") as outfile:
+        outfile.write(indexhtml)
+
+
+if __name__ == "__main__":
+    create_index()
---
+++ examples/run_all_models.py
@@ -0,0 +1,77 @@
+# Produce a showcase of all models. Assumes you have them all cached already locally.
+import os
+
+from loguru import logger
+
+import hordelib
+
+hordelib.initialise(setup_logging=False)
+
+from hordelib.horde import HordeLib
+from hordelib.shared_model_manager import SharedModelManager
+
+# Set this to where your hordelib tmp model cache is
+os.environ["AIWORKER_TEMP_DIR"] = "d:/temp/ray"
+
+# Do inference with all cached models
+VALIDATE_ALL_CACHED_MODELS = True
+
+
+def add_model(model_name):
+    logger.warning(f"Loading model {model_name}")
+    SharedModelManager.manager.load(model_name)
+    model_count = len(SharedModelManager.manager.compvis.get_loaded_models_names())
+    logger.warning(f"{model_count} models now loaded")
+
+
+def do_inference(model_name, iterations=1):
+    """Do some work on the GPU"""
+    horde = HordeLib()
+    for i in range(iterations):
+        data = {
+            "sampler_name": "k_euler",
+            "cfg_scale": 6.5,
+            "denoising_strength": 1.0,
+            "seed": 3688490319,
+            "height": 512,
+            "width": 512,
+            "karras": True,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": None,
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": (
+                "a woman closeup made out of metal, (cyborg:1.1), realistic skin, (detailed wire:1.3), "
+                "(intricate details), hdr, (intricate details, hyperdetailed:1.2), cinematic shot, "
+                "vignette, centered"
+            ),
+            "ddim_steps": 30,
+            "n_iter": 1,
+            "model": model_name,
+        }
+        pil_image = horde.basic_inference(data)
+        if not pil_image:
+            logger.error("Inference is failing to generate images")
+        else:
+            pil_image.save(f"images/all_models/{model_name}.webp", quality=90)
+
+
+def main():
+    HordeLib()
+    SharedModelManager.loadModelManagers(compvis=True)
+
+    os.makedirs("images/all_models/", exist_ok=True)
+
+    # We may have just fast-loaded a bunch of cached models, do some inference with each of them
+    if VALIDATE_ALL_CACHED_MODELS:
+        logger.warning("Validating cached model files")
+        for model in SharedModelManager.manager.get_loaded_models_names():
+            do_inference(model)
+        logger.warning("Model cache files validation completed.")
+        exit(0)
+
+
+if __name__ == "__main__":
+    main()
