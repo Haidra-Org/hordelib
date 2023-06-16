e3d248438bc5cc42601344f25fa806255597933d
tazlin <tazlin.on.github@gmail.com>
2023-06-15 20:48:56 -0400

    refactor: rework blip tests to `conftest.py` driven fixtures

--- tests/test_blip.py
+++ tests/test_blip.py
@@ -1,44 +1,36 @@
 # test_horde.py
 import pytest
-from clipfree.blip.caption import Caption
-from PIL import Image
+from PIL.Image import Image
 
-from hordelib.horde import HordeLib
+from hordelib.blip.caption import Caption
 from hordelib.shared_model_manager import SharedModelManager
 
 
 class TestHordeBlip:
     @pytest.fixture(autouse=True)
-    def setup_and_teardown(self):
-        self.horde = HordeLib()
-
-        self.default_model_manager_args = {
-            # aitemplate
+    def setup_and_teardown(
+        self,
+        shared_model_manager: type[SharedModelManager],
+    ):
+        default_model_manager_args = {
             "blip": True,
-            # "clip": True,
-            # "codeformer": True,
-            # "compvis": True,
-            # "controlnet": True,
-            # "diffusers": True,
-            # "esrgan": True,
-            # "gfpgan": True,
-            # "safety_checker": True,
         }
-        self.image = Image.open("images/test_db0.jpg")
-        SharedModelManager.loadModelManagers(**self.default_model_manager_args)
-        assert SharedModelManager.manager is not None
-        SharedModelManager.manager.load("BLIP_Large")
+        shared_model_manager.loadModelManagers(**default_model_manager_args)
+        shared_model_manager.manager.load("BLIP_Large")
+        assert shared_model_manager.manager.is_model_loaded("BLIP_Large") is True
         yield
-        del self.horde
-        SharedModelManager._instance = None
-        SharedModelManager.manager = None
+        shared_model_manager.manager.unload_model("BLIP_Large")
+        assert not shared_model_manager.manager.is_model_loaded("BLIP_Large")
 
-    def test_blip_large_caption(self):
-        assert SharedModelManager.manager.is_model_loaded("BLIP_Large") is True
-        model = SharedModelManager.manager.loaded_models["BLIP_Large"]
+    def test_blip_large_caption(
+        self,
+        shared_model_manager: type[SharedModelManager],
+        db0_test_image: Image,
+    ):
+        model = shared_model_manager.manager.loaded_models["BLIP_Large"]
         caption_class = Caption(model)
         caption = caption_class(
-            image=self.image,
+            image=db0_test_image,
             sample=True,
             num_beams=7,
             min_length=20,
