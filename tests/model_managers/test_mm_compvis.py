bc44d31bc4f9e9b41052ad9add02ce4b977ca3ec
tazlin <tazlin.on.github@gmail.com>
2023-06-15 20:48:42 -0400

    refactor: rework CompVisModelManger tests to `conftest.py` driven fixtures

---
+++ tests/model_managers/test_mm_compvis.py
@@ -0,0 +1,13 @@
+import pytest
+
+import hordelib
+from hordelib.model_manager.compvis import CompVisModelManager
+
+
+class TestCompvis:
+    @pytest.fixture(scope="class", autouse=True)
+    def compvis_model_manager(self, init_horde) -> CompVisModelManager:
+        yield CompVisModelManager()
+
+    def test_compvis_load_defaults(self, compvis_model_manager):
+        assert compvis_model_manager.load("Deliberate")
