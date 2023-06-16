e2d0849051707c981a5d3296f3fc3908abffb701
tazlin <tazlin.on.github@gmail.com>
2023-06-15 20:47:56 -0400

    refactor: fixture for safety_checker model manager

--- tests/model_managers/test_mm_safety_checker.py
+++ tests/model_managers/test_mm_safety_checker.py
@@ -1,26 +1,16 @@
 import pytest
 
-import hordelib
+from hordelib.model_manager.safety_checker import SafetyCheckerModelManager
 
 
 class TestSafetyChecker:
-    _initialised = False
+    @pytest.fixture(scope="class")
+    def isolated_safety_checker_model_manager(self, init_horde):
+        yield SafetyCheckerModelManager()
 
-    @pytest.fixture(autouse=True)
-    def setup_and_teardown(self):
-        if not self._initialised:
-            hordelib.initialise()
-        from hordelib.model_manager.safety_checker import SafetyCheckerModelManager
-
-        self.safety_model_manager = SafetyCheckerModelManager()
-        assert self.safety_model_manager is not None
-        yield
-        del self.safety_model_manager
-
-    def test_safety_checker_load_defaults(self):
-        success = self.safety_model_manager.load("safety_checker", cpu_only=True)
+    def test_safety_checker_load_defaults(
+        self,
+        isolated_safety_checker_model_manager: SafetyCheckerModelManager,
+    ):
+        success = isolated_safety_checker_model_manager.load("safety_checker", cpu_only=True)
         assert success
-
-    # def test_safety_checker_load_cpu_only(self):
-    #     success = self.safety_model_manager.load("safety_checker", cpu_only=True)
-    #     assert success
