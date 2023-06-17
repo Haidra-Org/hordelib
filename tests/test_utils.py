8054c04c53776ade1a2fe2c00b2964cc3d1c350b
tazlin <tazlin.on.github@gmail.com>
2023-06-17 09:01:12 -0400

    tests: some initial distance.py testing/validation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,10 @@
 import pytest
 
-import hordelib
-import hordelib.initialisation
 from hordelib.settings import UserSettings
+from hordelib.utils.distance import (
+    CosineSimilarityResultCode,
+    HistogramDistanceResultCode,
+)
 
 
 def test_worker_settings_singleton():
@@ -31,3 +33,18 @@ class TestWorkerSettingsWithInit:
     def test_worker_settings_set_get_vram(self, init_horde):
         UserSettings.set_vram_to_leave_free_mb("50%")
         assert UserSettings.get_vram_to_leave_free_mb() > 0
+
+
+class TestImageDistance:
+    def test_result_codes_in_order(self):
+        last_value = None
+        for result_code in CosineSimilarityResultCode:
+            if last_value is not None:
+                assert result_code > last_value, "CosineSimilarityResultCode values must be in *ascending* order"
+            last_value = result_code
+
+        last_value = None
+        for result_code in HistogramDistanceResultCode:
+            if last_value is not None:
+                assert result_code < last_value, "HistogramDistanceResultCode values must be in *descending* order"
+            last_value = result_code
