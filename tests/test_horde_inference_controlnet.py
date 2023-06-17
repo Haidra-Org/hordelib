1105a00f7ecc87d817eef1f66266eca9244f4c87
tazlin <tazlin.on.github@gmail.com>
2023-06-17 09:02:05 -0400

    refactor: rework controlnet inference tests to `conftest.py` driven fixtures

--- tests/test_horde_inference_controlnet.py
+++ tests/test_horde_inference_controlnet.py
@@ -6,35 +6,21 @@ from PIL import Image
 
 from hordelib.horde import HordeLib
 from hordelib.shared_model_manager import SharedModelManager
-from hordelib.utils.distance import are_images_identical
+
+from .testing_shared_functions import check_inference_image_similarity_pytest
 
 
 class TestHordeInference:
     @pytest.fixture(autouse=True)
-    def setup_and_teardown(self):
-        self.horde = HordeLib()
-
-        self.default_model_manager_args = {
-            # "codeformer": True,
-            "compvis": True,
-            "controlnet": True,
-            # "diffusers": True,
-            # "esrgan": True,
-            # "gfpgan": True,
-            # "safety_checker": True,
-        }
-        SharedModelManager.loadModelManagers(**self.default_model_manager_args)
-        assert SharedModelManager.manager is not None
-        SharedModelManager.manager.load("Deliberate")
+    def setup_and_teardown(self, shared_model_manager: type[SharedModelManager]):
         for preproc in HordeLib.CONTROLNET_IMAGE_PREPROCESSOR_MAP.keys():
-            SharedModelManager.manager.controlnet.download_control_type(preproc, ["stable diffusion 1"])
-        TestHordeInference.distance_threshold = int(os.getenv("IMAGE_DISTANCE_THRESHOLD", "100000"))
-        yield
-        del self.horde
-        SharedModelManager._instance = None
-        SharedModelManager.manager = None
+            shared_model_manager.manager.controlnet.download_control_type(preproc, ["stable diffusion 1"])
 
-    def test_controlnet_sd1(self):
+    def test_controlnet_sd1(
+        self,
+        shared_model_manager: type[SharedModelManager],
+        hordelib_instance: HordeLib,
+    ):
         data = {
             "sampler_name": "k_dpmpp_2m",
             "cfg_scale": 7.5,
@@ -56,27 +42,32 @@ class TestHordeInference:
             "source_image": Image.open("images/test_db0.jpg"),
             "source_processing": "img2img",
         }
-        assert self.horde is not None
+        assert hordelib_instance is not None
         for preproc in HordeLib.CONTROLNET_IMAGE_PREPROCESSOR_MAP.keys():
             if preproc == "scribble" or preproc == "mlsd":
                 # Skip
                 continue
             assert (
-                SharedModelManager.manager.controlnet.check_control_type_available(
+                shared_model_manager.manager.controlnet.check_control_type_available(
                     preproc,
                     "stable diffusion 1",
                 )
                 is True
             )
             data["control_type"] = preproc
-            pil_image = self.horde.basic_inference(data)
-            assert pil_image is not None
-            # assert get_image_distance(expected_filename, pil_image) < 100
-            img_filename = f"controlnet_{preproc}.png"
-            pil_image.save(f"images/{img_filename}", quality=100)
-            assert are_images_identical(f"images_expected/{img_filename}", pil_image, self.distance_threshold)
 
-    def test_controlnet_fake_cn(self):
+            pil_image = hordelib_instance.basic_inference(data)
+            assert pil_image is not None
+
+            img_filename = f"controlnet_{preproc}.png"
+
+            pil_image.save(f"images/{img_filename}", quality=100)
+            assert check_inference_image_similarity_pytest(
+                f"images_expected/{img_filename}",
+                pil_image,
+            )
+
+    def test_controlnet_fake_cn(self, hordelib_instance: HordeLib):
         data = {
             "sampler_name": "k_dpmpp_2m",
             "cfg_scale": 7.5,
@@ -98,12 +89,11 @@ class TestHordeInference:
             "source_image": Image.open("images/test_db0.jpg"),
             "source_processing": "img2img",
         }
-        assert self.horde is not None
+        assert hordelib_instance is not None
         with pytest.raises(Exception):
-            pil_image = self.horde.basic_inference(data)
-            assert pil_image is None
+            hordelib_instance.basic_inference(data)
 
-    def test_controlnet_strength(self):
+    def test_controlnet_strength(self, hordelib_instance: HordeLib):
         data = {
             "sampler_name": "k_dpmpp_2m",
             "cfg_scale": 7.5,
@@ -127,13 +117,19 @@ class TestHordeInference:
         }
         for strength in [1.0, 0.5, 0.2]:
             data["control_strength"] = strength
-            pil_image = self.horde.basic_inference(data)
-            assert pil_image is not None
-            img_filename = f"controlnet_strength_{strength}.png"
-            pil_image.save(f"images/{img_filename}", quality=100)
-            assert are_images_identical(f"images_expected/{img_filename}", pil_image, self.distance_threshold)
 
-    def test_controlnet_hires_fix(self):
+            pil_image = hordelib_instance.basic_inference(data)
+            assert pil_image is not None
+
+            img_filename = f"controlnet_strength_{strength}.png"
+
+            pil_image.save(f"images/{img_filename}", quality=100)
+            assert check_inference_image_similarity_pytest(
+                f"images_expected/{img_filename}",
+                pil_image,
+            )
+
+    def test_controlnet_hires_fix(self, hordelib_instance: HordeLib):
         data = {
             "sampler_name": "k_dpmpp_2m",
             "cfg_scale": 7.5,
@@ -158,8 +154,15 @@ class TestHordeInference:
         }
         for denoise in [0.4, 0.5, 0.6]:
             data["hires_fix_denoising_strength"] = denoise
-            pil_image = self.horde.basic_inference(data)
+
+            pil_image = hordelib_instance.basic_inference(data)
             assert pil_image is not None
+
             img_filename = f"controlnet_hires_fix_denoise_{denoise}.png"
+
             pil_image.save(f"images/{img_filename}", quality=100)
-            assert are_images_identical(f"images_expected/{img_filename}", pil_image, self.distance_threshold)
+
+            assert check_inference_image_similarity_pytest(
+                f"images_expected/{img_filename}",
+                pil_image,
+            )
