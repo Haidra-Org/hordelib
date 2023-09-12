0f8a296c4b8d8bf160e8e0361295a17d5afab07e
tazlin <tazlin.on.github@gmail.com>
2023-09-11 21:52:59 -0400

    feat: latest comfyui

--- hordelib/consts.py
+++ hordelib/consts.py
@@ -6,7 +6,7 @@ from strenum import StrEnum
 
 from hordelib.config_path import get_hordelib_path
 
-COMFYUI_VERSION = "e3d0a9a490195911d0c200f8dbde2991b7421678"
+COMFYUI_VERSION = "9562a6b49e63e63a16f3e45ff4965f72385f51fa"
 """The exact version of ComfyUI version to load."""
 
 REMOTE_PROXY = ""
