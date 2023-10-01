a43370f045830650a84ffe0829a7eb82f5469a3b
tazlin <tazlin.on.github@gmail.com>
2023-10-01 10:31:37 -0400

    fix: accurate download script

--- examples/download_all_sd_models.py
+++ examples/download_all_sd_models.py
@@ -3,7 +3,7 @@ from hordelib.shared_model_manager import SharedModelManager
 
 
 def main():
-    hordelib.initialisation.initialise(setup_logging=True, debug_logging=True)
+    hordelib.initialisation.initialise(setup_logging=True, logging_verbosity=5)
 
     SharedModelManager.load_model_managers(["compvis"])
     SharedModelManager.manager.compvis.download_all_models()
