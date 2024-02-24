19220a589076d8b8506bdffb4f3b68a2e65558fe
db0 <mail@dbzer0.com>
2024-02-24 17:43:16 +0100

    pass civitai token to checkpoint downloads

--- hordelib/model_manager/base.py
+++ hordelib/model_manager/base.py
@@ -599,6 +599,8 @@ class BaseModelManager(ABC):
 
             if "file_url" in download[i]:
                 download_url = download[i]["file_url"]
+                if self._civitai_api_token and self.is_model_url_from_civitai(download_url):
+                    download_url += f"?token={self._civitai_api_token}"
             if "file_name" in download[i]:
                 download_name = download[i]["file_name"]
             if "file_path" in download[i]:
