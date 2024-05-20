233077a941e51a460d0b8a32936949cce66d1e26
tazlin <tazlin.on.github@gmail.com>
2024-05-20 08:56:44 -0400

    feat: redirect std `logging` to loguru
    - ComfyUI uses stdlib `logging` to emit messages now (instead of print).
    - The original purpose of `OutputCollector` was to handle print specifically (and later, the TQDM messages for the purpose of emitting progress on inference so processes didn't appear stuck).
    - However, now that ComfyUI (and other libraries always have) use `logging` we can intercept those messages and emit them through loguru. See `comfy_horde.py` for more info.

--- hordelib/comfy_horde.py
+++ hordelib/comfy_horde.py
@@ -97,6 +97,39 @@ _uniformer: types.ModuleType
 
 
 # isort: off
+
+import logging
+
+
+class InterceptHandler(logging.Handler):
+    """
+    Add logging handler to augment python stdlib logging.
+
+    Logs which would otherwise go to stdlib logging are redirected through
+    loguru.
+    """
+
+    @logger.catch(default=True, reraise=True)
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists.
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message.
+        frame, depth = logging.currentframe(), 2
+        while frame and frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+# ComfyUI uses stdlib logging, so we need to intercept it.
+logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
+
+
 def do_comfy_import(
     force_normal_vram_mode: bool = False,
     extra_comfyui_args: list[str] | None = None,
