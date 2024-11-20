77f665cd6126979addf2129eb54c178e7ebeb5f6
tazlin <tazlin.on.github@gmail.com>
2024-11-20 11:29:37 -0500

    feat: check if training script is in a notebook

--- train.py
+++ train.py
@@ -44,7 +44,25 @@ import torch.nn as nn
 from optuna.terminator import EMMREvaluator, MedianErrorEvaluator, Terminator, TerminatorCallback
 from torch import optim
 from torch.utils.data import DataLoader, Dataset
-from tqdm import tqdm
+
+
+def is_notebook() -> bool:
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == "ZMQInteractiveShell":
+            return True  # Jupyter notebook or qtconsole
+        elif shell == "TerminalInteractiveShell":
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False  # Probably standard Python interpreter
+
+
+if is_notebook():
+    from tqdm.notebook import tqdm
+else:
+    from tqdm import tqdm
 
 from hordelib.horde import HordeLib
 
