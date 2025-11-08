f39bb04ec4869575bb02005e780636dcb0373676
tazlin <tazlin.on.github@gmail.com>
2025-11-08 13:35:30 -0500

    chore: remove (very) dated controlnet preprocessors

--- hordelib/nodes/comfy_controlnet_preprocessors/uniformer/mmcv/parallel/registry.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-import sys
-from torch.nn.parallel import DataParallel, DistributedDataParallel
-
-from hordelib.nodes.comfy_controlnet_preprocessors.uniformer.mmcv.utils import Registry
-
-MODULE_WRAPPERS = Registry("module wrapper", sys.modules[__name__])
-MODULE_WRAPPERS.register_module(module=DataParallel)
-MODULE_WRAPPERS.register_module(module=DistributedDataParallel)
