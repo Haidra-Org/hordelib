72aff10f18131034340057a15be833209720a8c1
db0 <mail@dbzer0.com>
2025-11-30 17:22:23 +0100

    feat: Support for Z-image turbo

--- tests/conftest.py
+++ tests/conftest.py
@@ -128,6 +128,7 @@ _sdxl_refined_model_name = "AlbedoBase XL (SDXL)"
 _stable_cascade_base_model_name = "Stable Cascade 1.0"
 _flux1_schnell_fp8_base_model_name = "Flux.1-Schnell fp8 (Compact)"
 _qwen_fp8_base_model_name = "Qwen-Image_fp8"
+_z_image_turbo_base_model_name = "Z-Image-Turbo"
 _am_pony_xl_model_name = "AMPonyXL"
 _rev_animated_model_name = "Rev Animated"
 
@@ -138,6 +139,7 @@ _all_model_names = [
     _stable_cascade_base_model_name,
     _flux1_schnell_fp8_base_model_name,
     _qwen_fp8_base_model_name,
+    _z_image_turbo_base_model_name,
     _am_pony_xl_model_name,
     _rev_animated_model_name,
 ]
@@ -181,6 +183,12 @@ def flux1_schnell_fp8_base_model_name(shared_model_manager: type[SharedModelMana
 def qwen_image_fp8_base_model_name(shared_model_manager: type[SharedModelManager]) -> str:
     """The default qwen-iimage fp8 model name used for testing."""
     return _qwen_fp8_base_model_name
+
+
+@pytest.fixture(scope="session")
+def z_image_turbo_base_model_name(shared_model_manager: type[SharedModelManager]) -> str:
+    """The default z-image turbo model name used for testing."""
+    return _z_image_turbo_base_model_name
 
 
 @pytest.fixture(scope="session")
